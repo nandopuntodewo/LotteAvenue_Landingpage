401461275094ee1ffba9dfbb0c8313962dcce5f4
nandopuntodewo <deanandropuntodewo@students.unnes.ac.id>
2024-01-17 20:26:21 +0700

    -

---
+++ LotteAvenue/src/Page/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavbarSimple from "../Components/Navbar";
+import About from "../Components/About";
+import Hero from "../Components/Hero";
+
+function Page() {
+
+    return (
+      <>
+        <NavbarSimple />
+        <Hero />
+        <About />
+      </>
+    );
+  }
+  
+  export default Page;
